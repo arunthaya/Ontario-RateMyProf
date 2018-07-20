b22e2b0344046dee79332168e0ac3ed4fad0ee93
arunthaya@gmail.com <arunthaya@gmail.com>
2018-07-20 00:45:03 -0400

    Tooltip showing
    - Tooltip showing over prof's names
    - Get rid of swear words for testing

--- chrome_samplextension/contentScript.js
+++ chrome_samplextension/contentScript.js
@@ -1,7 +1,32 @@
-var script = document.createElement('script');
+// var script = document.createElement('script');
+//
+// script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+// document.getElementsByTagName('head')[0].appendChild(script);
 
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
+
+const SCHEDULE_TYPES = [
+  "Lecture",
+  "Tutorial",
+  "Seminar",
+  "Discussion Group",
+  "Film Screening",
+  "Laboratory",
+  "Video on Demand",
+  "Directed Studies",
+  "Independent Study",
+  "Research Project"
+];
+
+let professorFilter = [];
+let professorFilterFiller = function(){
+  for(let i=0; i<100; i++){
+    let numberToPush = 11 + i*12;
+    professorFilter.push(numberToPush)
+  }
+  //console.log(professorFilter);
+};
+
+professorFilterFiller();
 
 // let temp = $("td").each(function() {
 //   if(this)
@@ -24,15 +49,62 @@ let counter2 = 0;
 //   }
 //   counter++;
 // });
+let filter1 = function(index){
+  return index == 1;
+};
 
-$('.pagebodydiv table').filter(
-  function(index){
-    return index == 1;
+let conciseFilter = function(index){
+  return index % 5 == 0;
+}
+
+let instructorFilter = function(index){
+  return professorFilter.includes(index);
+}
+
+//asdfasdf
+
+let jqueryFilterTest = function(){
+  let boolToReturn = false;
+  $(this).find('td').each(function() {
+    if(SCHEDULE_TYPES.includes($(this).text())){
+      boolToReturn = true;
+    }
+  });
+  return boolToReturn;
+}
+
+//let temp = document.body.innerHTML;
+//alert(temp.length);
+let USUCK = "hello";
+
+$('.pagebodydiv table').filter(filter1)
+  .find('tr').filter(
+    function(index){
+      return index == 4;
+    }
+  )
+  .css({
+    'background':'green'
   })
-  .find('table').each(function() {
-    counter++;
+  .find('table').find('tr')
+  .filter(jqueryFilterTest)
+  .find('td').filter(instructorFilter)
+  .qtip({
+    content: {
+      text: USUCK
+    },
+    position: {
+      at: 'top left'
+    },
+    hide: {
+      delay: 2000
+    }
+  })
+  .css({
+    'background':'yellow'
   });
 
-alert(counter);
+
+console.log(counter);
 
 //alert(counter2);
