8ecec2934b20763eaaf6acb25ded0cfc5fe48a36
arunthaya@gmail.com <arunthaya@gmail.com>
2018-07-19 22:14:28 -0400

    Barebone Script
    - Removal of background.js
    - Removal of popup.html
    - Removal of popup.js
    - These items will come back

--- chrome_samplextension/contentScript.js
+++ chrome_samplextension/contentScript.js
@@ -1,2 +1,38 @@
+var script = document.createElement('script');
 
-alert('hello');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// let temp = $("td").each(function() {
+//   if(this)
+//   $(this).css({'text-decoration':'underline'});
+// })
+// let counter = 0;
+// $('.pagebodydiv table').each(function() {
+//   if(counter == 1){
+//     $(this).css({'background':'blue'});
+//   }
+//   counter++;
+// })
+let counter = 0;
+let counter2 = 0;
+// $('.pagebodydiv table').each(function() {
+//   if(counter == 1){
+//     $(this).find('tr').each(function() {
+//       counter2++;
+//     });
+//   }
+//   counter++;
+// });
+
+$('.pagebodydiv table').filter(
+  function(index){
+    return index == 1;
+  })
+  .find('table').each(function() {
+    counter++;
+  });
+
+alert(counter);
+
+//alert(counter2);
