cf088a4a685bc99317895b36ce10755e938464bd
arunthaya@gmail.com <arunthaya@gmail.com>
2018-07-29 20:11:38 -0400

    Testing with extension

--- chrome_samplextension/popup.js
+++ chrome_samplextension/popup.js
@@ -1,14 +1,37 @@
 //let getRatings = document.getElementById('ratings');
 
-window.onload = function(){
-  document.getElementById('ratings').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-        {file: "contentScript.js"},
-        function(result){
-          console.log('uh duh herro');
-        }
-      );
-    });
+var buttonRetrieved;
+
+// let buttonGetter = function(){
+//   console.log('ran this function');
+//   buttonRetrieved = document.getElementById('ratings');
+// }
+//
+// window.onload = buttonGetter();
+
+let handleClick = function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      {file: "contentScript.js"},
+      function(result){
+        console.log('uh duh herro');
+      }
+    );
   });
 }
+
+window.onload = function() {
+  document.getElementById('ratings').addEventListener('click',handleClick);
+}
+
+// $('body').on('click', '#ratings', function () {
+//      console.log("yeahhhh!!! but this doesn't work for me :(");
+//      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//        chrome.tabs.executeScript(
+//          {file: "contentScript.js"},
+//          function(result){
+//            console.log('uh duh herro');
+//          }
+//        );
+//      });
+// });
