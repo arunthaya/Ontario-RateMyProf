32f215f3dcca99b8bfbd1892e4889cfeb5b20b2e
Arun Thayanithy <arunthaya@gmail.com>
2018-10-03 03:56:20 -0400

    Redesigned Options Page

--- chrome_samplextension/js/options.js
+++ chrome_samplextension/js/options.js
@@ -2,8 +2,17 @@
  * Function to save the default school to be used for the dropdown with extension
  */
 
+var toggledCards;
+
 function save_options(){
-  var school = parseInt(document.getElementById('school').value);
+  // var school = parseInt(document.getElementById('school').value);
+
+  if(toggledCards != null){
+    school = $(toggledCards).data("value");
+  } else {
+    school = 1420;
+  }
+  console.log(school);
   chrome.storage.sync.set({
     defaultSchool: school
   }, function(){
@@ -15,4 +24,24 @@ function save_options(){
   });
 }
 
-document.getElementById('save').addEventListener('click', save_options);
+//document.getElementById('save').addEventListener('click', save_options);
+
+
+$('.card').click(function(arg){
+  let prevToggledCard;
+  // console.log("toggledCard is ");
+  // console.log(toggledCards)
+  // console.log($(this).val());
+  //console.log("this is "+$(this));
+  if(toggledCards != $(this)){
+      prevToggledCard = toggledCards;
+      toggledCards = $(this);
+      toggledCards.toggleClass('is-checked');
+      if(prevToggledCard != null){
+        prevToggledCard.toggleClass('is-checked');
+      }
+      // console.log($(this).data("value"));
+  }
+  save_options();
+  // console.log('card-clicked');
+});
